8c33ccfe21d71958b0e0840ea22bc61910dad868
Nisha Bukke <sononenisha827@gmail.com>
2023-11-04 15:33:35 +0530

    added loader

--- javascript.js
+++ javascript.js
@@ -116,3 +116,21 @@ function jqUpdateSize(){
 }
 $(document).ready(jqUpdateSize);    // When the page first loads
 $(window).resize(jqUpdateSize);     // When the browser changes size
+
+
+
+// loader js
+
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+      document.querySelector(
+          "body").style.visibility = "hidden";
+      document.querySelector(
+          "#preloader").style.visibility = "visible";
+  } else {
+      document.querySelector(
+          "#preloader").style.display = "none";
+      document.querySelector(
+          "body").style.visibility = "visible";
+  }
+  };
